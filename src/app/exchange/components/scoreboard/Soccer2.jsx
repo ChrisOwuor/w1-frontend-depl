3c592f2b0faf5f6a1b1d4d2147680a6212d4074b
NathanNyawaya <nyawayanathan@gmail.com>
2024-10-20 10:38:13 +0000

    soccer

--- src/app/exchange/components/scoreboard/Soccer2.jsx
+++ src/app/exchange/components/scoreboard/Soccer2.jsx
@@ -8,11 +8,22 @@ const SoccerScores2 = ({ incidents, scores }) => {
   const onClick = () => {
     setOpen((prev) => !prev);
   };
+
+  // Get the elapsed time in minutes
+  const elapsedTime = scores ? scores.elapsedRegularTime : 0;
+
+  // Calculate the percentage fill for the first half and second half
+  const firstHalfElapsed = Math.min(elapsedTime, 45);
+  const secondHalfElapsed = Math.max(elapsedTime - 45, 0);
+
+  const firstHalfFill = (firstHalfElapsed / 45) * 100;
+  const secondHalfFill = (secondHalfElapsed / 45) * 100;
+
   return (
     <>
       {scores && (
-        <div className="bg-[#303131] flex flex-col items-start p-2">
-          <div className="flex items-center bg-[#303131] text-white">
+        <div className="bg-[#303131] flex flex-col items-start">
+          <div className="flex items-center bg-[#303131] text-white p-2">
             <p className="text-lg font-medium tracking-wider">
               {scores.score &&
                 `${scores.score.home.name}`}
@@ -30,13 +41,33 @@ const SoccerScores2 = ({ incidents, scores }) => {
                 `${scores.score.away.name}`}
             </p>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center p-2">
             <p className="text-lg font-medium tracking-wider">
               <span className="mr-2">{scores &&scores.matchStatus == "Finished" &&
                 `${scores.matchStatus}`}</span>
              <span className="text-success"> {scores &&
                 `${scores.elapsedRegularTime}'`}</span>
             </p>
+          </div>
+
+          <div className="w-full border-t border-white p-2 sm:p-4">
+            <div className="grid grid-cols-2 gap-4">
+              {/* First Half Progress Bar */}
+              <div className="bg-[#D2D2D2] h-1">
+                <div
+                  className="bg-[#04A807] h-1"
+                  style={{ width: `${firstHalfFill}%` }}
+                ></div>
+              </div>
+
+              {/* Second Half Progress Bar */}
+              <div className="bg-[#D2D2D2] h-1">
+                <div
+                  className="bg-[#04A807] h-1"
+                  style={{ width: `${secondHalfFill}%` }}
+                ></div>
+              </div>
+            </div>
           </div>
         </div>
       )}
